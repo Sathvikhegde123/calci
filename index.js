fdff60e5276ba99bb058debd31a30b3c925a3601
Sathvikhegde123 <sathvik.hegde17@gmail.com>
2025-04-15 16:32:15 +0530

    samkit did some changes

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ let input = document.getElementById("input");
 let clear = document.getElementById("clear");
 
 equal.addEventListener("click",() =>{
+    console.log("Button clicked")
     let inp_val = input.value;
     try{
         let ans = eval(inp_val);
         input.value = ans;
 
     }catch(err){
-        input.value = "Error";
+        alert("Error encountered");
+        input.value = "";
     }
 });
